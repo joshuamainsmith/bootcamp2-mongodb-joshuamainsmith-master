afdeec28bbd6cb408e98282137042aff58944065
Josh <32531221+joshuamainsmith@users.noreply.github.com>
2020-01-31 21:24:28 -0600

    Upload

---
+++ ListingSchema.js
@@ -0,0 +1,55 @@
+/* Import mongoose and define any variables needed to create the schema */
+import mongoose from 'mongoose';
+
+/* Create your schema for the data in the listings.json file that will define how data is saved in your database
+     See https://mongoosejs.com/docs/guide.html for examples for creating schemas
+     See also https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+  */
+
+
+const listingSchema = new mongoose.Schema({
+  /* Your code for a schema here */
+  //Check out - https://mongoosejs.com/docs/guide.html
+    code:
+    {
+        type: String, required: true, unique: true
+    },
+    name:
+    {
+        type: String, required: true, unique: false
+    },
+    coordinates:
+    {
+        latitude:
+        {
+            type: Number, required: false, unique: false
+        },
+        longitude:
+        {
+            type: Number, required: false, unique: false
+        }
+    },
+    address:
+    {
+        type: String, required: false, unique: false
+    }
+});
+
+listingSchema.pre('save', function (next)
+{
+    let date = new Date();
+
+    this.updated_at = date;
+
+    if (!this.created_at)
+        this.created_at = date;
+
+    next();
+});
+
+/* Use your schema to instantiate a Mongoose model
+Export the model to make it avaiable to other parts of your Node application */
+//Check out - https://mongoosejs.com/docs/guide.html#models
+let Listing = mongoose.model('Listing', listingSchema);
+export default mongoose.model('listings', listingSchema);
+
---
+++ queries.js
@@ -0,0 +1,109 @@
+/* Add all the required libraries*/
+
+/* Connect to your database using mongoose - remember to keep your key secret*/
+
+/* Fill out these functions using Mongoose queries*/
+//Check out - https://mongoosejs.com/docs/queries.html
+
+import mongoose from 'mongoose';
+import Listing from './ListingSchema.js';
+import config from './config.js';
+
+mongoose.connect(config.db.uri, { useNewUrlParser: true }).then(success =>
+{
+    console.log("Connected to DB");
+});
+
+const findLibraryWest = () =>
+{
+  /* 
+    Find the document that contains data corresponding to Library West,
+    then log it to the console. 
+   */
+    Listing.findOne({ "name": "Library West" }, function (err, listing)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Library West Log: " + JSON.stringify(listing, null, 4));
+        }
+    });
+};
+const removeCable = () =>
+{
+  /*
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
+    and remove this listing from your database and log the document to the console. 
+   */
+    Listing.findOne({ code: "CABL" }, function (err)
+    {
+        if (err)
+        {
+            console.log(err);
+        }
+    }).remove(function (err, listing)
+    {
+        if (!err)
+        {
+            console.log("CABL Deleted Listing " + JSON.stringify(listing, null, 4));
+        }
+    });
+};
+const updatePhelpsLab = () =>
+{
+  /*
+    Phelps Lab address is incorrect. Find the listing, update it, and then 
+    log the updated document to the console. 
+    
+    Correct Address: 1953 Museum Rd, Gainesville, FL 32603
+
+   */
+    Listing.findOne({ name: "Phelps Laboratory" }, function (err)
+    {
+        if (err)
+        {
+            console.log(err);
+
+        }
+
+    }).update({ address: "1953 Museum Rd, Gainesville, FL 32603" }, function (err)
+    {
+        if (err)
+        {
+            console.log(err);
+
+        }
+    }).then(function ()
+    {
+        Listing.findOne({ name: "Phelps Laboratory" }, function (listing)
+        {
+
+            console.log("Phelps Lab Updated Listing: " + listing);
+
+        })
+    });
+};
+const retrieveAllListings = () => {
+  /* 
+    Retrieve all listings in the database, and log them to the console. 
+   */
+    Listing.find(function (err, listings) {
+        if (err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("All DB Listings: " + JSON.stringify(listings, null, 4));
+        }
+    });
+};
+
+findLibraryWest();
+removeCable();
+updatePhelpsLab();
+retrieveAllListings();
